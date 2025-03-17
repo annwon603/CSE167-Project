11a3a40573dbc8d7f134b28b533216b418c7cfea
AnApostrophe <64292111+AnApostrophe@users.noreply.github.com>
2025-03-17 12:08:40 -0700

    Triangle

--- RayTracer-win-linux-setup/src/geometries/GeomTriangle.cpp
+++ RayTracer-win-linux-setup/src/geometries/GeomTriangle.cpp
@@ -32,6 +32,14 @@ std::vector<Intersection> GeomTriangle::intersect(Ray &ray) {
      * TODO: Implement the Ray intersection with the current geometry
      */
 
+    vec4 coords = inverse(mat4(vec4(vertices[0], 1), vec4(vertices[1], 1), vec4(vertices[2], 1), vec4(-ray.dir, 0))) * vec4(ray.p0, 1);
+
+    if (coords[0] >= 0 && coords[1] >= 0 && coords[2] >= 0) {
+        vec3 point = coords[0] * vertices[0] + coords[1] * vertices[1] + coords[2] * vertices[2];
+        vec3 normal = normalize(coords[0] * normals[0] + coords[1] * normals[1] + coords[2] * normals[2]);
+        intersections.push_back({ length(point - ray.p0), point, normal, this, nullptr});
+    }
+
     /**
      * Once you find the intersection, add it to the `intersections` vector.
      *
