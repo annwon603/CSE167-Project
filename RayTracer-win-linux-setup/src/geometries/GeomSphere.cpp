fff996af5fe15e43f5d408a2488537a72d2a31b7
annwon603 <aniwong603@gmail.com>
2025-03-17 21:41:40 -0700

    Fix Sphere?

--- RayTracer-win-linux-setup/src/geometries/GeomSphere.cpp
+++ RayTracer-win-linux-setup/src/geometries/GeomSphere.cpp
@@ -19,8 +19,35 @@ std::vector<Intersection> GeomSphere::intersect(Ray &ray) {
      * TODO: Implement the Ray intersection with the current geometry
      */
 
-    vec3 d = normalize(ray.dir);
-    float expression = pow(dot(d, ray.p0 - center), 2) - pow(length(ray.p0 - center), 2) + pow(radius, 2);
+    double t = 0.0f;
+    vec3 d = glm::normalize(ray.dir);
+
+    double addEnd1 = dot(-d , (ray.p0 - center));
+    double expression = pow(dot(d, ray.p0 - center), 2) - pow(length(ray.p0 - center),2) + pow(radius, 2);
+
+    if (expression < 0) {
+        return intersections;
+    }
+
+    if (expression == 0)
+    {
+        t = addEnd1;
+        vec3 point = ray.p0 + static_cast<float>(t) * d;
+        vec3 normal = glm::normalize(point - center);
+        intersections.push_back({ static_cast<float>(t), point, normal, this, nullptr });
+    }
+    else {
+        double square = sqrt(expression);
+        double posSum = addEnd1 + square;
+        double negSum = addEnd1 - square;
+        double tCurrent = (posSum < negSum) ? posSum : negSum;
+        vec3 point = ray.p0 + static_cast<float>(tCurrent) * d;
+        vec3 normal = glm::normalize(point - center);
+        intersections.push_back({ static_cast<float>(tCurrent) , point, normal, this , nullptr });
+
+    }
+
+   /* float expression = pow(dot(d, ray.p0 - center), 2) - pow(length(ray.p0 - center), 2) + pow(radius, 2);
 
     float tQuadPos = dot(-d, (ray.p0 - center)) + sqrt(expression);
     float tQuadNeg = dot(-d, (ray.p0 - center)) - sqrt(expression);
@@ -28,9 +55,9 @@ std::vector<Intersection> GeomSphere::intersect(Ray &ray) {
     if (expression >= 0) {
         float tCurrent = (tQuadPos < tQuadNeg) ? tQuadPos : tQuadNeg;
         vec3 point = ray.p0 + tCurrent * ray.dir;
-        vec3 normal = normalize(point - center);
+        vec3 normal = glm::normalize(point - center);
         intersections.push_back({ tCurrent, point, normal, this, nullptr });
-    }
+    }*/
 
     /**
      * Once you find the intersection, add it to the `intersections` vector.
