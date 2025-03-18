6d0356cbee548cab18e706a1e174e5fb3e4dfd80
AnApostrophe <64292111+AnApostrophe@users.noreply.github.com>
2025-03-17 21:39:53 -0700

    Triangle Intersection Half-way Fix

--- RayTracer-win-linux-setup/src/geometries/GeomTriangle.cpp
+++ RayTracer-win-linux-setup/src/geometries/GeomTriangle.cpp
@@ -9,7 +9,7 @@
 #include "Intersection.h"
 #include "Ray.h"
 
-GeomTriangle::GeomTriangle(std::vector<glm::vec3> &vertices, std::vector<glm::vec3> &normals) {
+GeomTriangle::GeomTriangle(std::vector<glm::vec3>& vertices, std::vector<glm::vec3>& normals) {
     this->vertices[0] = vertices[0];
     this->vertices[1] = vertices[1];
     this->vertices[2] = vertices[2];
@@ -19,21 +19,25 @@ GeomTriangle::GeomTriangle(std::vector<glm::vec3> &vertices, std::vector<glm::ve
     this->normals[2] = normals[2];
 }
 
-std::vector<Intersection> GeomTriangle::intersect(Ray &ray) {
+std::vector<Intersection> GeomTriangle::intersect(Ray& ray) {
     using namespace glm;
-
-    std::vector<Intersection> intersections;
     
-    // Barycentric coordinates
-    vec4 coords = inverse(mat4(vec4(vertices[0], 1.0), vec4(vertices[1], 1.0), vec4(vertices[2], 1.0), vec4(-ray.dir, 0.0))) * vec4(ray.p0, 1.0);
+    std::vector<Intersection> intersections;
 
-    // Add intersection with triangle
-    if (coords[0] >= 0 && coords[1] >= 0 && coords[2] >= 0) {
-        // Interpolated point and normal vectors
-        vec3 point = coords[0] * vertices[0] + coords[1] * vertices[1] + coords[2] * vertices[2];
-        vec3 normal = normalize(coords[0] * normals[0] + coords[1] * normals[1] + coords[2] * normals[2]);
+    mat4 transformationMatrix(vec4(vertices[0], 1.0), vec4(vertices[1], 1.0), vec4(vertices[2], 1.0), vec4(-ray.dir, 0.0));
+    vec4 rayCoords(ray.p0, 1.0);
 
-        intersections.push_back({ length(point - ray.p0), point, normal, this, nullptr});
+    vec4 intersectionCoords = inverse(transformationMatrix) * rayCoords;
+    vec3 lambdas(intersectionCoords[0], intersectionCoords[1], intersectionCoords[2]);
+
+    double t = intersectionCoords[3];
+
+    if (lambdas[0] >= 0 && lambdas[1] >= 0 && lambdas[2] >= 0 && t >= 0) {
+        vec3 intersectionPoint = lambdas[0] * vertices[0] + lambdas[1] * vertices[1] + lambdas[2] * vertices[2];
+        vec3 intersectionNormal = normalize(lambdas[0] * normals[0] + lambdas[1] * normals[1] + lambdas[2] * normals[2]);
+
+        intersections.push_back({static_cast<float>(t), intersectionPoint, intersectionNormal, this, nullptr});
     }
+
     return intersections;
 }
